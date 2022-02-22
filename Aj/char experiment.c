e3802f87798df88ab0fbb319a9e73323ca9cbb37
Ajay Kisku <99517907+Ajay-Kisku@users.noreply.github.com>
2022-02-22 06:35:44 +0530

    Add files via upload

---
+++ Aj/char experiment.c
@@ -0,0 +1,32 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+void charExperiment(){
+
+    char a;
+    printf("\nEnter char for a : ");
+    scanf(" %c",&a);
+
+    char b;
+    printf("\nEnter char for b : ");
+    scanf(" %c",&b);
+
+
+    char c;
+    printf("\nEnter char for c : ");
+    scanf(" %c",&c);
+
+
+    printf("\n\n%c%c%c",a,b,c);
+    printf("\n============================================================\n");
+
+}
+
+void main(){
+
+    charExperiment();
+    charExperiment();
+    charExperiment();
+
+}
